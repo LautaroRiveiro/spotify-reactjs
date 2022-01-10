f46e025e34ab5b7dc6fa4773686e60241e89ed52
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-01-10 16:01:06 -0300

    Page: Not found page

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import { NotFoundPageContainer } from './styles'
+
+const NotFoundPage = () => {
+
+  return (
+    <NotFoundPageContainer>
+      404
+    </NotFoundPageContainer>
+  )
+}
+
+export default NotFoundPage
---
+++ src/pages/NotFoundPage/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const NotFoundPageContainer = styled.div`
+  display: grid;
+  place-items: center;
+`
