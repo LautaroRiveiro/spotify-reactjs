6cd02103d727845d5bc8f52599d5a7bcbfd79985
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-01-10 16:29:27 -0300

    Recuperar el token del localstorage al iniciar

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,14 +1,18 @@
 import { createContext, useState } from "react"
+import { initApi } from "../services/services"
 
 export const AuthContext = createContext()
 export const AuthContextProvider = ({children})=>{
 
+  let _user = null
+  const access_token = JSON.parse(localStorage.getItem("access_token"))
+  if(access_token) {
+    initApi(access_token.access_token)
+    // TODO: Traer al usuario
+    _user = access_token
+  }
 
-  // TODO: Traer de localStorage
-  console.log("Esto se ejecuta en el AuthContext")
-  
-
-  const [ user, setUser ] = useState(null)
+  const [ user, setUser ] = useState(_user)
 
   const contextValue = {
     user,
