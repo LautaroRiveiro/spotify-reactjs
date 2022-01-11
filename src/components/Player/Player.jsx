5d25350148fe9c3bab5fd6b6806cedb89d0eebd9
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-01-11 20:47:25 -0300

    Player mejora estilos

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { usePlayer } from '../../context/usePlayer'
-import spotifyApi from '../../services/services'
 import { CurrentTrackInfo, PlayerContainer, ProgressBar, ProgressBarContainer, ProgressBarFill, TrackControls, TrackControlsContainer } from './styles'
 
 const Player = () => {
@@ -9,21 +8,23 @@ const Player = () => {
   const [duration, setDuration] = useState(null)
   const [contador, setContador] = useState(0)
 
+  if (!player.current) return <></>
+
   return (
     <PlayerContainer>
       <CurrentTrackInfo>
-        <img src="https://mosaic.scdn.co/300/ab67616d0000b2734ce8b4e42588bf18182a1ad2ab67616d0000b273608a63ad5b18e99da94a3f73ab67616d0000b273dbeec63ad914c973e75c24dfab67616d0000b273e230f303815e82a86713eedd" alt="" />
+        <img src={player.current.album?.images[1]?.url} alt="album-cover" />
         <div>
-          <span>nombre</span>
-          <span>artista</span>
+          <span>{player.current.name}</span>
+          <span>{player.current.artists.map(a => a.name).join(", ")}</span>
         </div>
         <span>❤</span>
       </CurrentTrackInfo>
       <TrackControlsContainer>
         <TrackControls>
-          <span onClick={()=>{console.log("play/pause");  player.togglePlay()}}>{player.isPlaying ? '⏸' : '▶'}</span>
-          <span onClick={()=>{console.log("repeat");  player.toggleLoop()}}>🔁</span>
-          <span>{ player.isLoop ? '◽' : '◾'}</span>
+          <span onClick={() => { console.log("play/pause"); player.togglePlay() }}>{player.isPlaying ? '⏸' : '▶'}</span>
+          <span onClick={() => { console.log("repeat"); player.toggleLoop() }}>🔁</span>
+          <span>{player.isLoop ? '◽' : '◾'}</span>
         </TrackControls>
         <ProgressBarContainer>
           <span>{contador}</span>
