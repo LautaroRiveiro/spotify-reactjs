a82928cfb4cfa2d6c4900ceb7eb3a7858fdf64e0
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2022-01-11 23:55:18 -0300

    Variables de entorno

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -3,9 +3,9 @@ import { LoginButton, LoginContainer } from './styles'
 
 const LoginPage = () => {
 
-  const CLIENT_ID = ''
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
   const LOGIN_SCOPE = 'user-read-private user-read-email'
-  const LOGIN_REDIRECT_URI = 'http://localhost:3000/callback'
+  const LOGIN_REDIRECT_URI = process.env.REACT_APP_LOGIN_REDIRECT_URI
   const LOGIN_STATE = generateRandomString(16)
   const LOGIN_URL = `https://accounts.spotify.com/authorize?response_type=token&client_id=${CLIENT_ID}&state=${LOGIN_STATE}&scope=${LOGIN_SCOPE}&redirect_uri=${LOGIN_REDIRECT_URI}`
 
